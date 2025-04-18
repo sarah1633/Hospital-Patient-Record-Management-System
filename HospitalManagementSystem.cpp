db4559df955a05ddf15e3c7376ce5e1a6a71187b
sarah1633 <sarahsebai.ss96@gmail.com>
2025-04-18 15:16:19 +0300

    Added patient struct for linked list -Sarah

--- HospitalManagementSystem.cpp
+++ HospitalManagementSystem.cpp
@@ -1,2 +1,13 @@
 #include <iostream>
 using namespace std;
+
+//Arrays and Linked Lists for patient records.
+
+struct Patient {
+    int id;
+    string name;
+    string condition;
+    int age;
+    Patient* next;
+};
+
