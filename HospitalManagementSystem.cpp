d697c873f2cec59b599cb08e31a2f474a9b64b86
sarah1633 <sarahsebai.ss96@gmail.com>
2025-04-18 16:54:57 +0300

    added main menu -Sarah
    added menu in main to maneuver through the code easily

--- HospitalManagementSystem.cpp
+++ HospitalManagementSystem.cpp
@@ -97,3 +97,84 @@ void viewTreatments(int id)
         cout << i + 1 << ". " << patient->treatments[i] << endl;
     }
 }
+
+int main()
+{
+    int choice;
+
+    while (true)
+    {
+        cout << "\n--- Hospital Patient Management ---\n";
+        cout << "1. Add Patient\n";
+        cout << "2. Search Patient by ID\n";
+        cout << "3. Add Treatment(or appointment) to Patient\n";
+        cout << "4. View Treatments(or appointments) of Patient\n";
+        cout << "5. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        if (choice == 1)
+        {
+            int id, age;
+            string name, condition;
+            cout << "Enter Patient ID: ";
+            cin >> id;
+            cout << "Enter Name: ";
+            cin >> name;
+            cout << "Enter Condition: ";
+            cin >> condition;
+            cout << "Enter Age: ";
+            cin >> age;
+            addPatient(id, name, condition, age);
+        }
+        else if (choice == 2)
+        {
+            int id;
+            cout << "Enter Patient ID to search: ";
+            cin >> id;
+            Patient *found = searchPatient(id);
+            if (found)
+            {
+                cout << "\nPatient Found:\n";
+                cout << "ID: " << found->id << "\n";
+                cout << "Name: " << found->name << "\n";
+                cout << "Condition: " << found->condition << "\n";
+                cout << "Age: " << found->age << "\n";
+                cout << "Treatments/appointments Count: " << found->treatmentCount << "\n";
+            }
+            else
+            {
+                cout << "Patient with ID " << id << " not found.\n";
+            }
+        }
+        else if (choice == 3)
+        {
+            int id;
+            string treatment;
+            cout << "Enter Patient ID: ";
+            cin >> id;
+            cin.ignore();
+            cout << "Enter Treatment(or appointment) Description: ";
+            getline(cin, treatment);
+            addTreatment(id, treatment);
+        }
+        else if (choice == 4)
+        {
+            int id;
+            cout << "Enter Patient ID: ";
+            cin >> id;
+            viewTreatments(id);
+        }
+        else if (choice == 5)
+        {
+            cout << "Exiting...\n";
+            break;
+        }
+        else
+        {
+            cout << "Invalid choice. Try again.\n";
+        }
+    }
+
+    return 0;
+}
