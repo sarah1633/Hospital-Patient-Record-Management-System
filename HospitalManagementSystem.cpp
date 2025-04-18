936c6a449312494ca7a9150efe00e0d88a022e3b
sarah1633 <sarahsebai.ss96@gmail.com>
2025-04-18 16:27:11 +0300

    added addTreatment and viewTreatments -Sarah
    change const int to define for memory.

--- HospitalManagementSystem.cpp
+++ HospitalManagementSystem.cpp
@@ -4,7 +4,7 @@ using namespace std;
 // Arrays and Linked Lists for patient records.//
 
 // struct to store patient info
-const int MAX_TREATMENTS = 20;
+#define MAX_TREATMENTS 100
 
 struct Patient
 {
@@ -12,7 +12,7 @@ struct Patient
     string name;
     string condition;
     int age;
-    string treatments[MAX_TREATMENTS];
+    string treatments[MAX_TREATMENTS]; // treatment or appointment
     int treatmentCount = 0;
     Patient *next;
 };
@@ -41,12 +41,59 @@ void addPatient(int id, string name, string condition, int age)
 }
 
 // Search for a patient by ID
-Patient* searchPatient(int id) {
-    Patient* temp = head;
-    while (temp) {
+Patient *searchPatient(int id)
+{
+    Patient *temp = head;
+    while (temp)
+    {
         if (temp->id == id)
             return temp;
         temp = temp->next;
     }
     return nullptr;
+}
+
+// Add a treatment(or appointment) to a patient
+void addTreatment(int id, string treatmentName)
+{
+    Patient *patient = searchPatient(id);
+
+    if (!patient)
+    {
+        cout << "Patient not found.\n";
+        return;
+    }
+
+    if (patient->treatmentCount >= MAX_TREATMENTS)
+    {
+        cout << "Max treatments reached for this patient.\n";
+        return;
+    }
+
+    patient->treatments[patient->treatmentCount++] = treatmentName;
+    cout << "Treatment '" << treatmentName << "' added to " << patient->name << ".\n";
+}
+
+// View all treatments(or appointments) for a patient
+void viewTreatments(int id)
+{
+    Patient *patient = searchPatient(id);
+
+    if (!patient)
+    {
+        cout << "Patient not found.\n";
+        return;
+    }
+
+    cout << "\nTreatment History for " << patient->name << ":\n";
+    if (patient->treatmentCount == 0)
+    {
+        cout << "No treatments recorded.\n";
+        return;
+    }
+
+    for (int i = 0; i < patient->treatmentCount; i++)
+    {
+        cout << i + 1 << ". " << patient->treatments[i] << endl;
+    }
 }
