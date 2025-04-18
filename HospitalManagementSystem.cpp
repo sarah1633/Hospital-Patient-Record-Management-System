8b534a7b5bd1d5eb471f5f68cb6aff04828473eb
sarah1633 <sarahsebai.ss96@gmail.com>
2025-04-18 15:34:23 +0300

    updated patient struct -Sarah
    need to use arrays to store appointments/treatments of each patient.

--- HospitalManagementSystem.cpp
+++ HospitalManagementSystem.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 using namespace std;
 
-//Arrays and Linked Lists for patient records.
+//Arrays and Linked Lists for patient records.//
+
+//struct to store patient info
+const int MAX_TREATMENTS = 20;
 
 struct Patient {
     int id;
     string name;
     string condition;
     int age;
+    string treatments[MAX_TREATMENTS];
+    int treatmentCount = 0;
     Patient* next;
 };
+
+// Head pointer for the patient linked list
+Patient* head = nullptr;
 
